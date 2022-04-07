70c9472ecdffcd51c0a5e70a7638403190e012b2
abv9359 <abv9359@gmail.com>
2022-04-07 21:55:31 +0530

    alumni tracking system

--- six-g.js
+++ six-g.js
@@ -6,10 +6,10 @@ app.set("view engine","ejs")
 app.use(bodyparser.urlencoded({extended:true}))
 app.listen(3000,function(){console.log("welcome to six-g")})
 app.get("/",function(req,res)
-{res.render("six-g")})
-mongoose.connect("mongodb+srv://Directorate:Abhi%40123@six-g.v9upn.mongodb.net/mydb?retryWrites=true&w=majority")
-alumnischema={rollno:Number ,name:String}
+{res.render("six-g", {user:"Anirudh"})})
+mongoose.connect("mongodb+srv://Directorate:Abhi%40123@six-g.v9upn.mongodb.net/SIX-G?retryWrites=true&w=majority")
+const alumnischema={name:String ,rollno:Number }
 const alumni=mongoose.model("alumni",alumnischema)
-const alumni1=new alumni({rollno:001, name:"xyz"})
+const alumni1=new alumni({rollno:003, name:"Abhinav"})
 alumni1.save()
 
